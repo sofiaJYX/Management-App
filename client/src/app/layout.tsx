8d4720e77890b9f8f444abcbfd9b1da5f4f7ef5c
Sofia Jia <97560867+sofiaJYX@users.noreply.github.com>
2024-12-19 18:40:18 -0400

    Create clerk sign in component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import { Suspense } from "react";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-dm-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,13 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${dmSans.className}`}>
-        <Providers>
-          <div className="root-layout">{children}</div>
-        </Providers>
-        
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${dmSans.className}`}>
+          <Providers>
+            <Suspense fallback={null}>
+              <div className="root-layout">{children}</div>
+            </Suspense>
+            <Toaster richColors closeButton />
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ client/src/components/SignIn.tsx
@@ -0,0 +1,8 @@
+import { SignIn } from "@clerk/nextjs"
+import React from 'react'
+
+const SignInComponent = () => {
+  return <SignIn />;
+}
+
+export default SignInComponent;
